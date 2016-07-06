c468107e1c04eccf6380b0a6c01724104a35320f
Code Ass <aycabta@gmail.com>
2016-07-06 18:35:32 +0900

    Use bearer token for Twitter

--- app.rb
+++ app.rb
@@ -66,10 +66,7 @@ class Bot < Sinatra::Base
 
   def init_twitter
     @twitter = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-      config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+      config.bearer_token        = ENV['TWITTER_BEARER_TOKEN']
     end
   end
 
