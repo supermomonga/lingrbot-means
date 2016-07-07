32d99a6e642de0cb3b96eeda6ec0677cea22df5c
Code Ass <aycabta@gmail.com>
2016-07-07 21:23:43 +0900

    Add multi URL tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -25,6 +25,12 @@ describe 'The Semantics=san' do
     expect(last_response.body).to eq('pong')
   end
 
+  it 'ping inside others' do
+    post '/', create_message_json("わたし\nping\n大好きです")
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('')
+  end
+
   it 'seiga' do
     post '/', create_message_json('http://seiga.nicovideo.jp/seiga/im5479269')
     expect(last_response).to be_ok
@@ -56,5 +62,16 @@ https://pbs.twimg.com/media/CZ4H6I5WcAAbBGo.jpg')
     post '/', create_message_json('http://www.pixiv.net/member_illust.php?mode=medium&illust_id=57723566')
     expect(last_response).to be_ok
     expect(last_response.body).to match(%r`^\[R-18\] 生命に危険を及ぼす程のオシャレ \(by 山田の性活が第一\)\nhttp://.+\.pixiv\.net/c/\d+x\d+/img-\w+/img/.+\.jpg$`)
+  end
+
+  it 'multi' do
+    post '/', create_message_json('http://www.pixiv.net/member_illust.php?mode=medium&illust_id=54003739 https://twitter.com/kumikumitm/status/693000092534587392')
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq("【サンプルと告知】志乃楓合同発行します【12/18　追記あり】 (by 紺@シンステ　レ-8)
+http://embed.pixiv.net/decorate.php?illust_id=54003739#.jpg
+Kumi TM (@kumikumitm) - 1RT / 5Fav 2016/01/29 18:17:52
+#ggjsap @raa0121 働いておる https://t.co/Czqc94p4yg
+https://pbs.twimg.com/media/CZ4H5jxWkAAHC6w.jpg
+https://pbs.twimg.com/media/CZ4H6I5WcAAbBGo.jpg")
   end
 end
