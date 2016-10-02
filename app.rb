5fc117d22ffdde4fd510bcaf19a7d850d151eb8f
Code Ass <aycabta@gmail.com>
2016-10-03 01:52:51 +0900

    Add kind of pixiv medium mode

--- app.rb
+++ app.rb
@@ -291,9 +291,16 @@ class Bot < Sinatra::Base
       r18 = res.at('.twitter-share-button').attr('data-text').include?('[R-18]')
       r18g = res.at('.twitter-share-button').attr('data-text').include?('[R-18G]')
       ugoila = res.at('.twitter-share-button').attr('data-text').include?('#うごイラ')
+      manga = !res.at('div.img-container a.manga').nil?
       title, author = meta.scan(/「([^」]+)」/).flatten
       rating = nil
+      kind = nil
       illust_url = nil
+      if ugoila
+        kind = 'うごイラ'
+      elsif manga
+        kind = '漫画'
+      end
       if r18
         if ugoila
           illust_url = res.at('.selected_works img').attr('src').gsub('128x128', '64x64')
@@ -316,7 +323,7 @@ class Bot < Sinatra::Base
         end
         illust_url = append_extension(illust_url)
       end
-      "#{rating.nil? ? '' : "[#{rating}] "}#{title} (by #{author})#{illust_url.nil? ? '' : "\n#{illust_url}"}"
+      "#{rating.nil? ? '' : "[#{rating}] "}#{title} (by #{author})#{kind.nil? ? '' : " #{kind}"}#{illust_url.nil? ? '' : "\n#{illust_url}"}"
     end
   end
 
